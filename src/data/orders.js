1b12ea38831cad5329a428239210ceb1a5d6ccb9
Dominik Kundel <dominik.kundel@gmail.com>
2018-03-22 22:14:48 +0100

    :construction: Fetch orders on initialization

--- src/data/orders.js
+++ src/data/orders.js
@@ -1,4 +1,8 @@
+import SyncClient from 'twilio-sync';
 import EventEmitter from 'event-emitter-es6';
+
+const tokenUrl = 'https://dramatic-sidewalk-3548.twil.io/barista-token';
+const orderListName = 'orders';
 
 let instance;
 export default class Orders extends EventEmitter {
@@ -10,5 +14,35 @@ export default class Orders extends EventEmitter {
   constructor() {
     super();
     this.client = undefined;
+    this.orderList = undefined;
+    this.orders = [];
+  }
+
+  async init() {
+    if (this.client) {
+      return this.client;
+    }
+
+    const resp = await fetch(tokenUrl);
+    if (!resp.ok) {
+      throw new Error('Could not fetch token');
+    }
+
+    const { token } = await resp.json();
+    this.client = new SyncClient(token);
+    this.orderList = await this.client.list(orderListName);
+    this.orders = await this.fetchOrders();
+    return this.orders;
+  }
+
+  async fetchOrders() {
+    const page = await this.orderList.getItems({ pageSize: 1000 });
+    return page.items.map(item => {
+      return {
+        number: item.index,
+        order: item.value.order,
+        status: item.value.status
+      };
+    });
   }
 }
