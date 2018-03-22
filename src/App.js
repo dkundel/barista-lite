fa17ab2c3cdbac2cf463689588ac36f97e1a31d1
Dominik Kundel <dominik.kundel@gmail.com>
2018-03-22 21:35:02 +0100

    :construction: Consume OrderEntry to test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import OrderEntry from './components/OrderEntry';
+
 class App extends Component {
+  constructor(props, ctx) {
+    super(props, ctx);
+    this.finishOrder.bind(this);
+    this.cancelOrder.bind(this);
+  }
+
+  finishOrder(orderNumber) {
+    console.log('Finish');
+  }
+
+  cancelOrder(orderNumber) {
+    console.log('Cancel');
+  }
+
   render() {
     return (
       <div role="main">
         <h1>Barista</h1>
+        <div className="order-list">
+          <OrderEntry
+            orderNumber={0}
+            onCancel={this.cancelOrder}
+            onFinished={this.finishOrder}
+          >
+            Hello
+          </OrderEntry>
+        </div>
       </div>
     );
   }
