c011b9297d0ce9e8c7163b78fd51965bfe2f5a6e
Dominik Kundel <dominik.kundel@gmail.com>
2018-03-23 21:12:04 +0100

    :art: Add code comments to sync update function

--- twilio-functions/sync-update-handler.js
+++ twilio-functions/sync-update-handler.js
@@ -1,6 +1,7 @@
 // Create a Twilio function and configure it as webhook
 exports.handler = function(context, event, callback) {
-  console.log(event);
+  // We are only interest in the item update events of the orders list
+  // Exit early for other events
   if (
     event.EventType !== 'list_item_updated' ||
     event.ListUniqueName !== 'orders'
@@ -9,9 +10,12 @@ exports.handler = function(context, event, callback) {
     return;
   }
 
+  // Parse the data of the list item
   const { status, phoneNumber, order } = JSON.parse(event.ItemData);
   const orderNumber = event.ItemIndex;
 
+  // If the status changed to anything that isn't
+  // finished/cancelled we'll ignore it
   if (status !== 'finished' && status !== 'cancelled') {
     callback(null, undefined);
     return;
@@ -19,10 +23,12 @@ exports.handler = function(context, event, callback) {
 
   let message = `Your order #${orderNumber} for ${order} has been ${status}`;
 
+  // Send an SMS to the person who ordered the coffee
   const twilioClient = context.getTwilioClient();
   twilioClient.messages
     .create({ from: context.PHONE_NUMBER, to: phoneNumber, body: message })
     .then(x => {
+      // If the SMS was successfully sent, remove the item from the list
       return twilioClient.sync
         .services(event.ServiceSid)
         .syncLists(event.ListUniqueName)
@@ -30,7 +36,7 @@ exports.handler = function(context, event, callback) {
         .remove();
     })
     .then(x => {
-      callback(null, 'Sent');
+      callback(null, undefined);
     })
     .catch(err => callback(err));
 };
