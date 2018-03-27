86ff77b2c7381816656ae2446e6fbbe56a3309e9
Dominik Kundel <dominik.kundel@gmail.com>
2018-03-27 15:19:21 +0200

    :lipstick: Remove index.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'milligram/dist/milligram.css';
 
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
